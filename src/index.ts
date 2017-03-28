bddb32601a52a8d8dd1c8269cb2e8c92e336f0cb
Pap Lőrinc <paplorinc@gmail.com>
2017-03-28 15:28:54 +0300

    Made index exportable

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import * as Hapi from 'hapi'
-import {truncate} from "./truncator";
+import {truncate} from "./truncator"
 
-const server = new Hapi.Server()
-server.connection({
-    host: 'localhost',
-    port: 8000
-})
-server.route({
-    method: 'GET',
-    path: '/truncate/{number}',
-    handler: (req, resp) => resp(truncate(req.params.number))
-})
-server.start((err) => {
-    if (err) throw err
-    else console.log('Server running at:', server.info.uri)
-})
+
+export function init() {
+    const port = process.env.port || 8000
+    const server = new Hapi.Server()
+
+    server.connection({
+        host: 'localhost',
+        port: port,
+    })
+    server.route({
+        method: 'GET',
+        path: '/truncate/{number}',
+        handler: (req, resp) => resp(truncate(req.params.number))
+    })
+    server.start(err => {
+        if (err) throw err
+        else console.log('Server running at:', server.info.uri)
+    })
+
+    return server
+}
