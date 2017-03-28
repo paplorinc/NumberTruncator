2ff1c5a2ad5438090fc8cb75b02d85b13d4dfa45
Pap Lőrinc <paplorinc@gmail.com>
2017-03-28 15:29:38 +0300

    Added tests and fixed discovered errors

--- src/truncator.ts
+++ src/truncator.ts
@@ -1,91 +1,88 @@
-import std = require("tstl");
+import std = require("tstl")
 
-import TreeMap = std.TreeMap;
 class ScaleNames {
-    private static map = new TreeMap<number, string>([
-        std.make_pair(6, 'Million'),
-        std.make_pair(9, 'Billion'),
-        std.make_pair(12, 'Trillion'),
-        std.make_pair(15, 'Quadrillion'),
-        std.make_pair(18, 'Quintillion'),
-        std.make_pair(21, 'Sextillion'),
-        std.make_pair(24, 'Septillion'),
-        std.make_pair(27, 'Octillion'),
-        std.make_pair(30, 'Nonillion'),
-        std.make_pair(33, 'Decillion'),
-        std.make_pair(36, 'Undecillion'),
-        std.make_pair(39, 'Duodecillion'),
-        std.make_pair(42, 'Tredecillion'),
-        std.make_pair(45, 'Quattuordecillion'),
-        std.make_pair(48, 'Quinquadecillion'),
-        std.make_pair(51, 'Sedecillion'),
-        std.make_pair(54, 'Septendecillion'),
-        std.make_pair(57, 'Octodecillion'),
-        std.make_pair(60, 'Novendecillion'),
-        std.make_pair(63, 'Vigintillion'),
-        std.make_pair(66, 'Unvigintillion'),
-        std.make_pair(69, 'Duovigintillion'),
-        std.make_pair(72, 'Tresvigintillion'),
-        std.make_pair(75, 'Quattuorvigintillion'),
-        std.make_pair(78, 'Quinquavigintillion'),
-        std.make_pair(81, 'Sesvigintillion'),
-        std.make_pair(84, 'Septemvigintillion'),
-        std.make_pair(87, 'Octovigintillion'),
-        std.make_pair(90, 'Novemvigintillion'),
-        std.make_pair(93, 'Trigintillion'),
-        std.make_pair(96, 'Untrigintillion'),
-        std.make_pair(99, 'Duotrigintillion'),
-        std.make_pair(102, 'Trestrigintillion'),
-        std.make_pair(105, 'Quattuortrigintillion'),
-        std.make_pair(108, 'Quinquatrigintillion'),
-        std.make_pair(111, 'Sestrigintillion'),
-        std.make_pair(114, 'Septentrigintillion'),
-        std.make_pair(117, 'Octotrigintillion'),
-        std.make_pair(120, 'Noventrigintillion'),
-        std.make_pair(123, 'Quadragintillion'),
-        std.make_pair(153, 'Quinquagintillion'),
-        std.make_pair(183, 'Sexagintillion'),
-        std.make_pair(213, 'Septuagintillion'),
-        std.make_pair(243, 'Octogintillion'),
-        std.make_pair(273, 'Nonagintillion'),
-        std.make_pair(303, 'Centillion'),
-        std.make_pair(306, 'Uncentillion'),
-        std.make_pair(309, 'Duocentillion')
+    private static map = new std.TreeMap<number, string>([
+        [6, 'Million'],
+        [9, 'Billion'],
+        [12, 'Trillion'],
+        [15, 'Quadrillion'],
+        [18, 'Quintillion'],
+        [21, 'Sextillion'],
+        [24, 'Septillion'],
+        [27, 'Octillion'],
+        [30, 'Nonillion'],
+        [33, 'Decillion'],
+        [36, 'Undecillion'],
+        [39, 'Duodecillion'],
+        [42, 'Tredecillion'],
+        [45, 'Quattuordecillion'],
+        [48, 'Quinquadecillion'],
+        [51, 'Sedecillion'],
+        [54, 'Septendecillion'],
+        [57, 'Octodecillion'],
+        [60, 'Novendecillion'],
+        [63, 'Vigintillion'],
+        [66, 'Unvigintillion'],
+        [69, 'Duovigintillion'],
+        [72, 'Tresvigintillion'],
+        [75, 'Quattuorvigintillion'],
+        [78, 'Quinquavigintillion'],
+        [81, 'Sesvigintillion'],
+        [84, 'Septemvigintillion'],
+        [87, 'Octovigintillion'],
+        [90, 'Novemvigintillion'],
+        [93, 'Trigintillion'],
+        [96, 'Untrigintillion'],
+        [99, 'Duotrigintillion'],
+        [102, 'Trestrigintillion'],
+        [105, 'Quattuortrigintillion'],
+        [108, 'Quinquatrigintillion'],
+        [111, 'Sestrigintillion'],
+        [114, 'Septentrigintillion'],
+        [117, 'Octotrigintillion'],
+        [120, 'Noventrigintillion'],
+        [123, 'Quadragintillion'],
+        [153, 'Quinquagintillion'],
+        [183, 'Sexagintillion'],
+        [213, 'Septuagintillion'],
+        [243, 'Octogintillion'],
+        [273, 'Nonagintillion'],
+        [303, 'Centillion'],
+        [306, 'Uncentillion']
     ])
 
     public static getMap() {
         let notYetTruncated = () => ScaleNames.map.begin().second.length > 1
         if (notYetTruncated()) {
-            let isUnique = (prefix: string) => {
-                for (let it = ScaleNames.map.begin(); it != ScaleNames.map.end(); it.next())
-                    if (it.second.substr(0, prefix.length) == prefix)
+            let isUnique = (value: string, prefix: string) => {
+                for (let it = ScaleNames.map.begin(); it != ScaleNames.map.end(); it = it.next())
+                    if (it.second != value && it.second.startsWith(prefix))
                         return false
                 return true
             }
 
-            for (let it = ScaleNames.map.begin(); it != ScaleNames.map.end(); it.next()) {
+            for (let it = ScaleNames.map.begin(); it != ScaleNames.map.end(); it = it.next())
                 for (let i = 1; i < it.second.length; i++) {
                     let prefix = it.second.substr(0, i)
-                    if (isUnique(prefix)) {
+                    if (isUnique(it.second, prefix)) {
                         ScaleNames.map.set(it.first, prefix)
                         break
                     }
                 }
-            }
         }
         return ScaleNames.map
     }
 }
 
+let getScale = (num: string) => 1 + Math.floor(Math.log10(Math.abs(Number(num))))
+
 export let truncate = (num: string) => {
-    let originalScale = num.length;
-    let truncatedScale = ScaleNames.getMap().lower_bound(originalScale).first // TODO less than million
-    return Number(num) / (originalScale - truncatedScale)
+    let numScale = getScale(num)
+    let truncatedScale = ScaleNames.getMap().lower_bound(numScale).prev().value
+    if (truncatedScale == null) return Number(num)
+    else {
+        let decimal = Number(num) / 10 ** truncatedScale.first
+        let truncatedDecimal = Math.trunc(10 * decimal) / 10
+        return truncatedDecimal + truncatedScale.second
+    }
 }
-
-
-// negative numbers
-// smaller than million
-// same abbreviation for Quadrillion, Quintillion (i.e. Long.MAX_VALUE (9223372036854775807L) is nine Quintillion, but 9.2Q could also mean Quadrillion)
-// Double.MAX_VALUE (1.7 * 10^308) is greater than 1 Centillion, should it be displayed as e.g. 1100C?
-// what should happen for gaps, e.g. 10^123 = 'Quadragintillion', 10^153 = 'Quinquagintillion'
--- test/truncator.test.ts
+++ test/truncator.test.ts
@@ -1,6 +1,98 @@
-import * as chai from 'chai'
+import * as chai from "chai"
+import {IServerInjectResponse} from "hapi"
 
-describe('truncate', () => {
-    it('should initialize the map', () => chai.assert(, '')
-    )
+const server = require('../src/index').init()
+
+describe('truncator specifications: should keep anything smaller than a million', () => {
+    it('3.14', done =>
+        assertEquals(done, '3.14', '3.14'))
+    it('3.141592653589793', done =>
+        assertEquals(done, '3.141592653589793', '3.141592653589793'))
+
+    it('532', done =>
+        assertEquals(done, '532', '532'))
+
+    it('1.234e+3', done =>
+        assertEquals(done, '1.234e+3', '1234'))
+
+    it('-123456.789', done =>
+        assertEquals(done, '-123456.789', '-123456.789'))
 })
+
+describe('truncator specifications: should abbreviate millions', () => {
+    it('1000000', done =>
+        assertEquals(done, '1000000', '1M'))
+    it('1100000', done =>
+        assertEquals(done, '1100000', '1.1M'))
+    it('1234567', done =>
+        assertEquals(done, '1234567', '1.2M'))
+    it('1290000', done =>
+        assertEquals(done, '1290000', '1.2M'))
+    it('1999999', done =>
+        assertEquals(done, '1999999', '1.9M'))
+    it('87654321', done =>
+        assertEquals(done, '87654321', '87.6M'))
+    it('2500000.34', done =>
+        assertEquals(done, '2500000.34', '2.5M'))
+
+    it('1.234567e+7', done =>
+        assertEquals(done, '1.234567e+7', '12.3M'))
+    it('1.23e-2', done =>
+        assertEquals(done, '1.23e-2', '0.0123'))
+
+    it('-1.234567e+7', done =>
+        assertEquals(done, '-1.234567e+7', '-12.3M'))
+
+    it('-987654321', done =>
+        assertEquals(done, '-987654321', '-987.6M'))
+})
+
+describe('truncator specifications: should abbreviate billions and greater', () => {
+    it('1123456789', done =>
+        assertEquals(done, '1123456789', '1.1B'))
+
+    it('1123456789', done =>
+        assertEquals(done, '1123456789', '1.1B'))
+
+    // double precision lost
+    it('1.2e+119', done =>
+        assertEquals(done, '1.2e+119', '119.9Octot'))
+    it('1.2e+120', done =>
+        assertEquals(done, '1.2e+120', '1.1Novent'))
+    it('1.2e+121', done =>
+        assertEquals(done, '1.2e+121', '11.9Novent'))
+    it('1.2e+122', done =>
+        assertEquals(done, '1.2e+122', '119.9Novent'))
+    it('1.2e+123', done =>
+        assertEquals(done, '1.2e+123', '1.1Quadra'))
+
+    // double precision lost
+    it('1.2e+152', done =>
+        assertEquals(done, '1.2e+152', '1.1999999999999997e+29Quadra'))
+    it('1.2e+153', done =>
+        assertEquals(done, '1.2e+153', '1.1Quinquag'))
+
+    it('Number.MAX_VALUE', done =>
+        assertEquals(done, '1.7976931348623157e+308', '179.7Unc'))
+
+    it('-1123456789.345678', done =>
+        assertEquals(done, '-1123456789.345678', '-1.1B'))
+})
+
+const assertEquals = function (done: () => any, input: string, expected: string) {
+    server.inject({method: 'GET', url: '/truncate/' + input}, (res: IServerInjectResponse) => {
+        chai.assert.equal(res.result, expected)
+        done()
+    })
+}
+
+// negative numbers
+// smaller than million
+// should it work for scientific notation?
+// same abbreviation for Quadrillion, Quintillion (i.e. Long.MAX_VALUE (9223372036854775807L) is nine Quintillion, but 9.2Q could also mean Quadrillion)
+// * abbreviate to shortest prefix
+// * upper-camel case abbreviation?
+// Double.MAX_VALUE (1.7 * 10^308) is greater than 1 Centillion, should it be displayed as e.g. 1100C?
+// what should happen for gaps, e.g. 10^123 = 'Quadragintillion', 10^153 = 'Quinquagintillion'
+// should 900M be 0.9B instead?
+// should I avoid numeric roundings for e.g. double??
