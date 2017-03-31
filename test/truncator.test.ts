38136b9ff052681c1f83d4145242bf4fb0c6a33f
Pap Lőrinc <paplorinc@gmail.com>
2017-03-31 09:41:40 +0300

    Changed tests to async/await

--- test/truncator.test.ts
+++ test/truncator.test.ts
@@ -1,63 +1,59 @@
-import * as chai from "chai"
-import {IServerInjectResponse} from "hapi"
+import * as chai from "chai";
+import {IServerInjectResponse} from "hapi";
 import {truncate} from "../src/truncator";
 import server from "../src/index";
 
-const assert = chai.assert
+const expect = chai.expect
 
 describe('truncator specifications: should keep anything invalid or smaller than a million', () => {
-    it('via `get`', done => assertEquals(done, '532', '532'))
+    it('via `get`', done => expectEq(done, '532', '532'))
 
-    it('via method call', done => {
-        assert(truncate('0') == '0')
-        assert(truncate('3.14') == '3.14')
-        assert(truncate('3.141592653589793') == '3.141592653589793')
-        assert(truncate('-123456.789') == '-123456.789')
+    it('via method call', async () => {
+        expect(await truncate('0')).eq('0');
+        expect(await truncate('3.14')).eq('3.14')
+        expect(await truncate('3.141592653589793')).eq('3.141592653589793')
+        expect(await truncate('-123456.789')).eq('-123456.789')
 
-        assert(truncate("") == "")
-        assert(truncate("invalid") == "invalid")
-        assert(truncate(NaN.toString()) == NaN.toString())
-        assert(truncate(Infinity.toString()) == Infinity.toString())
-        assert(truncate((-Infinity).toString()) == (-Infinity).toString())
-
-        done()
+        expect(await truncate('')).eq('')
+        expect(await truncate('invalid')).eq('invalid')
+        expect(await truncate(NaN.toString())).eq(NaN.toString())
+        expect(await truncate(Infinity.toString())).eq(Infinity.toString())
+        expect(await truncate((-Infinity).toString())).eq((-Infinity).toString())
     })
 })
 
 describe('truncator specifications: should abbreviate millions', () => {
-    it('via `get`', done => assertEquals(done, '1000000', '1M'))
+    it('via `get`', done => expectEq(done, '1000000', '1M'))
 
-    it('via method call', done => {
-        assert(truncate('1100000') == '1.1M')
-        assert(truncate('1234567') == '1.2M')
-        assert(truncate('1290000') == '1.2M')
-        assert(truncate('1999999') == '1.9M')
-        assert(truncate('2500000.34') == '2.5M')
-        assert(truncate('87654321') == '87.6M')
-        assert(truncate('-987654321') == '-987.6M')
-        done()
+    it('via method call', async () => {
+        expect(await truncate('1100000')).eq('1.1M')
+        expect(await truncate('1234567')).eq('1.2M')
+        expect(await truncate('1290000')).eq('1.2M')
+        expect(await truncate('1999999')).eq('1.9M')
+        expect(await truncate('2500000.34')).eq('2.5M')
+        expect(await truncate('87654321')).eq('87.6M')
+        expect(await truncate('-987654321')).eq('-987.6M')
     })
 })
 
 describe('truncator specifications: should abbreviate billions and greater', () => {
-    it('via `get`', done => assertEquals(done, '1123456789', '1.1B'))
+    it('via `get`', done => expectEq(done, '1123456789', '1.1B'))
 
-    it('via method call', done => {
-        assert(truncate('-1234567891.2345') == '-1.2B')
-        assert(truncate('1234567893123') == '1.2Tril')
-        assert(truncate('12345678931234') == '12.3Tril')
-        assert(truncate('123456789312345') == '123.4Tril')
-        assert(truncate('1234567893123456') == '1.2Quadri')
-        assert(truncate('12345678931234567') == '12.3Quadri')
-        assert(truncate('123456789312345678') == '123.4Quadri')
-        assert(truncate('1234567893123456789') == '1.2Quint')
-        done()
+    it('via method call', async () => {
+        expect(await truncate('-1234567891.2345')).eq('-1.2B')
+        expect(await truncate('1234567893123')).eq('1.2Tril')
+        expect(await truncate('12345678931234')).eq('12.3Tril')
+        expect(await truncate('123456789312345')).eq('123.4Tril')
+        expect(await truncate('1234567893123456')).eq('1.2Quadri')
+        expect(await truncate('12345678931234567')).eq('12.3Quadri')
+        expect(await truncate('123456789312345678')).eq('123.4Quadri')
+        expect(await truncate('1234567893123456789')).eq('1.2Quint')
     })
 })
 
-const assertEquals = (done: () => any, input: string, expected: string) => {
+const expectEq = (done: () => any, input: string, expected: string) => {
     server.inject({method: 'GET', url: '/truncate/' + input}, (res: IServerInjectResponse) => {
-        chai.assert.equal(res.result, expected)
+        expect(res.result).eq(expected)
         done()
     })
 }
