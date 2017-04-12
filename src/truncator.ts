5f1ac9c510109087d6f74c11560dcf989751f493
Pap Lőrinc <paplorinc@gmail.com>
2017-04-12 23:28:29 +0300

    Simplified code

--- src/truncator.ts
+++ src/truncator.ts
@@ -2,12 +2,12 @@ import {getScale, isValid, pow10, truncateNumber} from './numberutils'
 import {getScaleSuffix} from './numericscales'
 
 export const truncate = (num: string) => {
-    if (!isValid(num)) return num
-    else {
+    if (isValid(num)) {
         const scaleAndSuffix = getScaleSuffix(getScale(num))
-        if (scaleAndSuffix) return format(Number(num), scaleAndSuffix)
-        else return num
+        if (scaleAndSuffix)
+            return format(Number(num), scaleAndSuffix)
     }
+    return num
 }
 
 const format = (num: number, [scale, suffix]: [number, string]) => {
